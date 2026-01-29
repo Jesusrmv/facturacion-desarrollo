dfc06d014d9101ebb61a370d2283e66e44a2f696
Jesus Moreno <jrafael.mv26@gmail.com>
2026-01-28 21:11:45 -0500

    Configuracion del proyecto, acceso a BBS, configuracion lombok y la creacion del bean modelmapper

---
+++ src/main/java/com/jesus/facturacion_desarrollo/FacturacionDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.facturacion_desarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionDesarrolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesus/facturacion_desarrollo/FacturacionDesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.facturacion_desarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionDesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
