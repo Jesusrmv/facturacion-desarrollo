b8d76cc4d0951466c142a9f4cf226a030cd7b0b5
Jesus Moreno <jrafael.mv26@gmail.com>
2026-02-05 21:17:55 -0500

    se creo la logica de guardado para las facturas

--- src/main/java/com/jesus/facturacion_desarrollo/service/FacturaService.java
+++ src/main/java/com/jesus/facturacion_desarrollo/service/FacturaService.java
@@ -1,14 +1,24 @@
 package com.jesus.facturacion_desarrollo.service;
 
+import com.jesus.facturacion_desarrollo.dto.RequestDetalleFacturaDTO;
+import com.jesus.facturacion_desarrollo.dto.RequestFacturaDTO;
 import com.jesus.facturacion_desarrollo.dto.ResponseFacturaDTO;
+import com.jesus.facturacion_desarrollo.model.DetalleFactura;
+import com.jesus.facturacion_desarrollo.model.Factura;
+import com.jesus.facturacion_desarrollo.model.Producto;
 import com.jesus.facturacion_desarrollo.repository.DetalleFacturaRepository;
 import com.jesus.facturacion_desarrollo.repository.FacturaRepository;
+import com.jesus.facturacion_desarrollo.repository.ProductoRepository;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -17,12 +27,12 @@ public class FacturaService {
 
 
     private final FacturaRepository facturaRepository;
-    private final FacturaRepository detalleFacturaRepository;
+    private final ProductoRepository productoRepository;
     private final ModelMapper modelMapper;
 
-    public FacturaService(FacturaRepository facturaRepository, FacturaRepository detalleFacturaRepository, ModelMapper modelMapper) {
+    public FacturaService(FacturaRepository facturaRepository, ProductoRepository productoRepository, ModelMapper modelMapper) {
         this.facturaRepository = facturaRepository;
-        this.detalleFacturaRepository = detalleFacturaRepository;
+        this.productoRepository = productoRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -45,4 +55,41 @@ public class FacturaService {
         facturaRepository.deleteById(id);
 
     }
+@Transactional
+    public ResponseFacturaDTO save(RequestFacturaDTO requestFacturaDTO) {
+        Factura factura = new Factura();
+        BigDecimal subtotalFactura = BigDecimal.ZERO;
+        Set<DetalleFactura> detalles = new HashSet<>();
+
+        factura.setNumeroFactura(requestFacturaDTO.getNumeroFactura());
+
+        for (RequestDetalleFacturaDTO detalleFacturaDTO: requestFacturaDTO.getDetallesFactura()) {
+            //Producto producto = productoRepository.findById(detalleFacturaDTO.getIdProducto()).get();
+            Producto producto = productoRepository.findById(detalleFacturaDTO.getIdProducto()).orElseThrow(
+                    ()-> new RuntimeException("Producto no encontrado")
+            );
+
+
+            BigDecimal totalProducto = producto.getPrecio().multiply(BigDecimal.valueOf(detalleFacturaDTO.getCantidad()));
+            subtotalFactura = subtotalFactura.add(totalProducto);
+
+            DetalleFactura detalleFactura = new DetalleFactura();
+            detalleFacturaDTO.setIdProducto(detalleFacturaDTO.getIdProducto());
+            detalleFactura.setPrecio(subtotalFactura);
+            detalleFactura.setCantidad(detalleFacturaDTO.getCantidad());
+            detalleFactura.setTotal(totalProducto);
+            detalleFactura.setFactura(factura);
+
+            detalles.add(detalleFactura);
+        }
+
+        factura.setDetalleFacturas(detalles);
+        factura.setSubTotal(subtotalFactura);
+        factura.setTotal(subtotalFactura.add(subtotalFactura.multiply(BigDecimal.valueOf(factura.getIVA()))));
+
+        Factura savedFactura = facturaRepository.save(factura);
+
+        return modelMapper.map(savedFactura, ResponseFacturaDTO.class);
+    }
+
 }
