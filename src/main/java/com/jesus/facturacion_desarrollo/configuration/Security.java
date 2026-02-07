62bdb21b803a175d7af06d4d3e452e3bd4d82a77
Jesus Moreno <jrafael.mv26@gmail.com>
2026-02-07 11:36:52 -0500

    Se agrego Spring security y se valido el permiso para el api

---
+++ src/main/java/com/jesus/facturacion_desarrollo/configuration/Security.java
@@ -0,0 +1,24 @@
+package com.jesus.facturacion_desarrollo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)  throws Exception {
+        httpSecurity.csrf(
+                crsf -> crsf.disable()
+        ).authorizeHttpRequests(
+                auth ->{
+                    auth.requestMatchers("/api/v1/productos/**").permitAll()
+                            .anyRequest().authenticated();
+                }
+        ).httpBasic(Customizer.withDefaults());
+
+    return httpSecurity.build();
+    }
+}
